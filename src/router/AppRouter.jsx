9525126176f3584e73b6342454022272489fc2e4
Viet281101 <nva281101@gmail.com>
2025-02-20 14:25:05 +0700

    update

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 import ButtonsPage from "../pages/ButtonsPage";
 import CheckboxPage from "../pages/CheckboxPage";
+import RadioButtonPage from "../pages/RadioButtonPage";
 import SwitcherPage from "../pages/SwitcherPage";
 
 const AppRouter = () => {
@@ -10,11 +11,13 @@ const AppRouter = () => {
       <nav style={{ display: "flex", gap: "15px", padding: "10px", background: "#f4f4f4" }}>
         <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Buttons</NavLink>
         <NavLink to="/checkboxes" className={({ isActive }) => (isActive ? "active" : "")}>Checkboxes</NavLink>
+        <NavLink to="/radio-buttons" className={({ isActive }) => (isActive ? "active" : "")}>Radio Buttons</NavLink>
         <NavLink to="/switchers" className={({ isActive }) => (isActive ? "active" : "")}>Switchers</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<ButtonsPage />} />
         <Route path="/checkboxes" element={<CheckboxPage />} />
+        <Route path="/radio-buttons" element={<RadioButtonPage />} />
         <Route path="/switchers" element={<SwitcherPage />} />
       </Routes>
     </Router>
