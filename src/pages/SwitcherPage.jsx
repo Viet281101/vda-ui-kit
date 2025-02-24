8a604789d99298c34080266f22d572d3997d1404
Viet281101 <nva281101@gmail.com>
2025-02-24 14:14:05 +0700

    complete switcher components

--- src/pages/SwitcherPage.jsx
+++ src/pages/SwitcherPage.jsx
@@ -33,6 +33,30 @@ const SwitcherPage = () => {
             <td><Switcher defaultChecked={true} onChange={setIsSwitcherOn} /></td>
             <td><Switcher label="Text option" position="labelInfo" defaultChecked={true} onChange={setIsSwitcherOn} /></td>
           </tr>
+
+          {/* Disabled / Off */}
+          <tr>
+            <td>Disabled / Off</td>
+            <td><Switcher label="Text option" disabled /></td>
+            <td><Switcher disabled /></td>
+            <td><Switcher label="Text option" position="labelInfo" disabled /></td>
+          </tr>
+
+          {/* Disabled / On */}
+          <tr>
+            <td>Disabled / On</td>
+            <td><Switcher label="Text option" disabled defaultChecked={true} /></td>
+            <td><Switcher disabled defaultChecked={true} /></td>
+            <td><Switcher label="Text option" position="labelInfo" disabled defaultChecked={true} /></td>
+          </tr>
+
+          {/* Error */}
+          <tr>
+            <td>Error</td>
+            <td><Switcher label="Text option" error /></td>
+            <td><Switcher error /></td>
+            <td><Switcher label="Text option" position="labelInfo" error /></td>
+          </tr>
         </tbody>
       </table>
     </div>
