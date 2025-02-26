e9aa0b52150795a26a18562a293db22219b990fb
Viet281101 <nva281101@gmail.com>
2025-02-26 13:52:05 +0700

    fixed password components

--- src/pages/PasswordPage.jsx
+++ src/pages/PasswordPage.jsx
@@ -22,37 +22,37 @@ const PasswordPage = () => {
           {/* Default State */}
           <tr>
             <td>Default</td>
+            <td><PasswordInput placeholder="Password" defaultVisible={true} /></td>
             <td><PasswordInput placeholder="Password" /></td>
-            <td><PasswordInput placeholder="Password" isVisible={false} /></td>
           </tr>
           {/* Focused State */}
           <tr>
             <td>Focused</td>
-            <td><PasswordInput label="Password" placeholder="Password" alwaysFocused={true} /></td>
+            <td><PasswordInput label="Password" placeholder="Password" alwaysFocused={true} defaultVisible={true} /></td>
             <td><PasswordInput label="Password" placeholder="Password" alwaysFocused={true} /></td>
           </tr>
           {/* Completed State */}
           <tr>
             <td>Completed</td>
-            <td><PasswordInput label="Password" placeholder="Password" showCounter={true} alwaysShowLabel={true} /></td>
+            <td><PasswordInput label="Password" placeholder="Password" showCounter={true} alwaysShowLabel={true} defaultVisible={true} /></td>
             <td><PasswordInput label="Password" placeholder="Password" showCounter={true} alwaysShowLabel={true} /></td>
           </tr>
           {/* Error State */}
           <tr>
             <td>Error</td>
-            <td><PasswordInput label="Password" placeholder="Password" error={true} /></td>
+            <td><PasswordInput label="Password" placeholder="Password" error={true} defaultVisible={true} /></td>
             <td><PasswordInput label="Password" placeholder="Password" error={true} /></td>
           </tr>
           {/* Disabled State */}
           <tr>
             <td>Disabled</td>
-            <td><PasswordInput label="Password" placeholder="Password" disabled={true} /></td>
+            <td><PasswordInput label="Password" placeholder="Password" disabled={true} defaultVisible={true} /></td>
             <td><PasswordInput label="Password" placeholder="Password" disabled={true} /></td>
           </tr>
           {/* Disabled Completed State */}
           <tr>
             <td>Disabled Completed</td>
-            <td><PasswordInput label="Password" placeholder="Password" disabled={true} defaultValue="MyPassword123" /></td>
+            <td><PasswordInput label="Password" placeholder="Password" disabled={true} defaultValue="MyPassword123" defaultVisible={true} /></td>
             <td><PasswordInput label="Password" placeholder="Password" disabled={true} defaultValue="MyPassword123" /></td>
           </tr>
         </tbody>
